3ba7a376b4d2d25df216d846422b188009152b5b
Rota1001 <johnny1001s000602@gmail.com>
2024-06-09 07:27:52 +0800

    last page

--- lib/pages/pair_page.dart
+++ lib/pages/pair_page.dart
@@ -11,9 +11,29 @@ class _PairPageState extends State<PairPage> {
   @override
   Widget build(BuildContext context) {
     return  Container(
+        alignment: Alignment.topCenter,
         decoration: const BoxDecoration(
             color: Color(0xFF457B9D)
-        )
+        ),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(5,45,0,0),
+            alignment: Alignment.topLeft,
+            child:  IconButton(
+                icon: const Icon(
+                    Icons.arrow_back,
+                    size: 48,
+                    color: Color(0xFFF1FAEE)
+                ),
+                onPressed: (){
+                  Navigator.pop(context, true);
+                }
+            )
+          )
+
+        ],
+      )
     );
   }
 }
